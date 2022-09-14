eee5d1ef62ec5b2710cd7e2cdf9f9b1244497db8
Ryo Nakamura <r7kamura@gmail.com>
2022-09-14 11:44:16 +0900

    Remove unnecessary autoload that is no longer used

--- lib/rucoa.rb
+++ lib/rucoa.rb
@@ -22,7 +22,6 @@ module Rucoa
   autoload :Position, 'rucoa/position'
   autoload :Range, 'rucoa/range'
   autoload :Rbs, 'rucoa/rbs'
-  autoload :RbsDocumentLoader, 'rucoa/rbs_document_loader'
   autoload :Rubocop, 'rucoa/rubocop'
   autoload :Server, 'rucoa/server'
   autoload :Source, 'rucoa/source'
