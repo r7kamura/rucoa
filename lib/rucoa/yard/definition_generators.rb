b20a8c2eb3d45adca7afcbc4610fa1ca208dfd36
Ryo Nakamura <r7kamura@gmail.com>
2022-09-24 09:35:01 +0900

    Refactor autoload sections

--- lib/rucoa/yard/definition_generators.rb
+++ lib/rucoa/yard/definition_generators.rb
@@ -3,10 +3,9 @@
 module Rucoa
   module Yard
     module DefinitionGenerators
-      autoload :Base, 'rucoa/yard/definition_generators/base'
-
       autoload :AttributeReaderDefinitionGenerator, 'rucoa/yard/definition_generators/attribute_reader_definition_generator'
       autoload :AttributeWriterDefinitionGenerator, 'rucoa/yard/definition_generators/attribute_writer_definition_generator'
+      autoload :Base, 'rucoa/yard/definition_generators/base'
       autoload :ClassDefinitionGenerator, 'rucoa/yard/definition_generators/class_definition_generator'
       autoload :ConstantAssignmentDefinitionGenerator, 'rucoa/yard/definition_generators/constant_assignment_definition_generator'
       autoload :MethodDefinitionGenerator, 'rucoa/yard/definition_generators/method_definition_generator'
